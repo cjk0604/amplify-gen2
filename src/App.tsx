dded9f098af238ec51207a43b44a1fe561e29537
amplify user <kimcj@amazon.com>
2024-08-09 10:38:09 +0900

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     <Authenticator>
     {({ signOut, user }) => (
       <main>
-        <h1>My todos for {user?.username}</h1>
+        <h1>My todos for {user?.signInDetails?.loginId}</h1>
         <button onClick={createTodo}>+ new</button>
         <ul>
           {todos.map((todo) => (
